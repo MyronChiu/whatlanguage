11f90860dd7df54ea2514ff223824d2cf1da83f5
MyronChiu <myron.gc@gmail.com>
2013-10-28 17:48:43 -0700

    trying to test

--- test/test_whatlanguage.rb
+++ test/test_whatlanguage.rb
@@ -9,75 +9,71 @@ class TestWhatLanguage < Test::Unit::TestCase
   end
   
   def test_string_method
-    assert_equal :english, "This is a test".language
+    assert_equal :EN, "This is a test".language
   end
 
   def test_arabic
-    assert_equal :arabic, @wl.language("اللغة التي هي هذه؟")
+    assert_equal :AR, @wl.language("اللغة التي هي هذه؟")
   end
 
   def test_dutch
-    assert_equal :dutch, @wl.language("Als hadden geweest is, is hebben te laat.")
+    assert_equal :NL, @wl.language("Als hadden geweest is, is hebben te laat.")
   end
 
   def test_farsi
-    assert_equal :farsi, @wl.language("وقتی مادرم به من آموخت که به آواز خواندن.")
+    assert_equal :FA, @wl.language("وقتی مادرم به من آموخت که به آواز خواندن.")
   end
 
   def test_finnish
-    assert_equal :finnish, @wl.language("Mitä kieltä tämä on?")
+    assert_equal :FI, @wl.language("Mitä kieltä tämä on?")
   end
 
   def test_french
-    assert_equal :french, @wl.language("Bonjour, je m'appelle Sandrine. Voila ma chatte.")
+    assert_equal :FR, @wl.language("Bonjour, je m'appelle Sandrine. Voila ma chatte.")
   end
   
   def test_german
-    assert_equal :german, @wl.language("Welche Sprache ist das?")
+    assert_equal :DE, @wl.language("Welche Sprache ist das?")
   end
 
   def test_greek
-    assert_equal :greek, @wl.language("Ποια γλώσσα είναι αυτή;")
-  end
-
-  def test_hebrew
-    assert_equal :hebrew, @wl.language("באיזו שפה זה?")
+    assert_equal :EL, @wl.language("Ποια γλώσσα είναι αυτή;")
   end
 
   def test_hungarian
-    assert_equal :hungarian, @wl.language("Milyen nyelv ez?")
+    assert_equal :HU, @wl.language("Milyen nyelv ez?")
   end
 
   def test_italian
-    assert_equal :italian, @wl.language("Roma, capitale dell'impero romano, è stata per secoli il centro politico e culturale della civiltà occidentale.")
+    assert_equal :IT, @wl.language("Roma, capitale dell'impero romano, è stata per secoli il centro politico e culturale della civiltà occidentale.")
   end
 
   def test_korean
-    assert_equal :korean, @wl.language("이 어떤 언어인가?")
+    assert_equal :KR, @wl.language("이 어떤 언어인가?")
   end
 
   def test_norwegian
-    assert_equal :norwegian, @wl.language("Hvilket språk er dette?")
+    assert_equal :NO, @wl.language("Hvilket språk er dette?")
   end
 
   def test_polish
-    assert_equal :polish, @wl.language("W jakim języku to jest?")
+    assert_equal :PL, @wl.language("W jakim języku to jest?")
   end
 
   def test_portuguese
-    assert_equal :portuguese, @wl.language("Que linguagem é essa?")
+    assert_equal :PT, @wl.language("Que linguagem é essa?")
   end
 
   def test_russian
-    assert_equal :russian, @wl.language("Все новости в хронологическом порядке")
+    assert_equal :RU, @wl.language("Все новости в хронологическом порядке")
   end
 
   def test_spanish
-    assert_equal :spanish, @wl.language("La palabra mezquita se usa en español para referirse a todo tipo de edificios dedicados.")
+    assert_equal :ES, @wl.language("La palabra mezquita se usa en español para referirse a todo tipo de edificios dedicados.")
   end
 
   def test_swedish
-    assert_equal :swedish, @wl.language("Vilket språk är detta?")
+    assert_equal :SV, @wl.language("Vilket språk är detta?")
   end
   
   def test_nothing
@@ -92,22 +88,18 @@ class TestWhatLanguage < Test::Unit::TestCase
     assert_kind_of Hash, @wl.process_text("this is a test")
   end
 
-  def test_italian
-    assert_equal :italian, @wl.language("Roma, capitale dell'impero romano, è stata per secoli il centro politico e culturale della civiltà occidentale.")
-  end
-
   def test_language_selection
-    selective_wl = WhatLanguage.new(:german, :english)
-    assert_equal :german, selective_wl.language("der die das")
+    selective_wl = WhatLanguage.new(:DE, :EN)
+    assert_equal :DE, selective_wl.language("der die das")
   end
 
   def test_language_selection_empty
     selective_wl = WhatLanguage.new()
-    assert_equal :russian, selective_wl.language("Все новости в хронологическом порядке")
+    assert_equal :RU, selective_wl.language("Все новости в хронологическом порядке")
   end
 
   def test_language_selection_mixed
-    selective_wl = WhatLanguage.new(:german, :all, :english)
-    assert_equal :russian, selective_wl.language("Все новости в хронологическом порядке")
+    selective_wl = WhatLanguage.new(:DE, :all, :EN)
+    assert_equal :RU, selective_wl.language("Все новости в хронологическом порядке")
   end
 end
